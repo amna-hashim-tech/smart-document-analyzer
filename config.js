791e14e14f4f9a76fa953f75bced32892682ae8a
Amna Hashim <amnahashim.contact@gmail.com>
2025-08-14 19:37:03 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const endpoint = "https://<your-resource-name>.cognitiveservices.azure.com/";
-export const apiKey = process.env.FORM_RECOGNIZER_API_KEY;
+export const endpoint = "https://doc-analyzer-amna.cognitiveservices.azure.com/";
+export const apiKey = process.env.AZURE_FORM_RECOGNIZER_KEY;
