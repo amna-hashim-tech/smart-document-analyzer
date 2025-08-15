706f119b4a71090c139b52fc77f830cef0332638
Amna Hashim <amnahashim.contact@gmail.com>
2025-08-15 10:42:43 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,7 @@
-export const endpoint = "https://doc-analyzer-amna.cognitiveservices.azure.com/";
-export const apiKey = process.env.AZURE_FORM_RECOGNIZER_KEY;
+// config.js
+// DO NOT put your real Azure key here!
+
+module.exports = {
+  endpoint: process.env.AZURE_FORM_RECOGNIZER_ENDPOINT || "https://your-resource-name.cognitiveservices.azure.com/",
+  apiKey: process.env.AZURE_FORM_RECOGNIZER_KEY || "fake-key-for-testing-only"
+};
